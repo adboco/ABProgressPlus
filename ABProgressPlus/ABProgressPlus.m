bb0d20271b0e791edcfde7e6733fe6c71ce0b4df
adboco <adboco@telefonica.net>
2016-03-01 16:14:37 +0100

    Fix

--- ABProgressPlus/ABProgressPlus.m
+++ ABProgressPlus/ABProgressPlus.m
@@ -11,35 +11,51 @@
 @implementation ABProgressPlus
 
 + (void)setDefaultMaskType:(ABProgressPlusMaskType)maskType{
-    [super setDefaultMaskType:maskType];
+    [super setDefaultMaskType:(SVProgressHUDMaskType)maskType];
 }
 
 + (void)setDefaultAnimationType:(ABProgressPlusAnimationType)animationType{
-    [super setDefaultAnimationType:animationType];
+    [super setDefaultAnimationType:(DGActivityIndicatorAnimationType)animationType];
 }
 
 + (void)showWithAnimationType:(ABProgressPlusAnimationType)animType {
-    [super showWithAnimationType:animType];
+    [super showWithAnimationType:(DGActivityIndicatorAnimationType)animType];
 }
 
 + (void)showWithMaskType:(ABProgressPlusMaskType)maskType {
-    [super showWithMaskType:maskType];
+    [super showWithMaskType:(SVProgressHUDMaskType)maskType];
 }
 
 + (void)showWithAnimationType:(ABProgressPlusAnimationType)animType maskType:(ABProgressPlusMaskType)maskType {
-    [super showWithAnimationType:animType maskType:maskType];
+    [super showWithAnimationType:(DGActivityIndicatorAnimationType)animType maskType:(SVProgressHUDMaskType)maskType];
 }
 
 + (void)showWithStatus:(NSString *)status animType:(ABProgressPlusAnimationType)animType {
-    [super showWithStatus:status animType:animType];
+    [super showWithStatus:status animType:(DGActivityIndicatorAnimationType)animType];
 }
 
 + (void)showWithStatus:(NSString*)status maskType:(ABProgressPlusMaskType)maskType {
-    [super showWithStatus:status maskType:maskType];
+    [super showWithStatus:status maskType:(SVProgressHUDMaskType)maskType];
 }
 
 + (void)showWithStatus:(NSString *)status animType:(ABProgressPlusAnimationType)animType maskType:(ABProgressPlusMaskType)maskType {
-    [super showWithStatus:status animType:animType maskType:maskType];
+    [super showWithStatus:status animType:(DGActivityIndicatorAnimationType)animType maskType:(SVProgressHUDMaskType)maskType];
+}
+
++ (void)showSuccessWithStatus:(NSString *)string maskType:(ABProgressPlusMaskType)maskType {
+    [super showSuccessWithStatus:string maskType:(SVProgressHUDMaskType)maskType];
+}
+
++ (void)showErrorWithStatus:(NSString *)string maskType:(ABProgressPlusMaskType)maskType {
+    [super showErrorWithStatus:string maskType:(SVProgressHUDMaskType)maskType];
+}
+
++ (void)showInfoWithStatus:(NSString *)string maskType:(ABProgressPlusMaskType)maskType {
+    [super showInfoWithStatus:string maskType:(SVProgressHUDMaskType)maskType];
+}
+
++ (void)showImage:(UIImage *)image status:(NSString *)string maskType:(ABProgressPlusMaskType)maskType {
+    [super showImage:image status:string maskType:(SVProgressHUDMaskType)maskType];
 }
 
 @end
